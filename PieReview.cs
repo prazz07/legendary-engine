c3162440a6e5eaa4e3ac99bf766bbba441f795a8
pranjal agarwal <60132656+prazz07@users.noreply.github.com>
2020-02-18 10:47:26 +0530

    Delete PieReview.cs

--- PieReview.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BethanysPieShop.Models
-{
-    public class PieReview
-    {
-        public int PieReviewId { get; set; }
-        public Pie Pie { get; set; }
-        public string Review { get; set; }
-    }
-}
